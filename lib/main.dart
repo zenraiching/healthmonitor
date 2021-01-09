9dc8e9e5986381554a33169bab446679e02d238c
zenraiching <rickdavidsangalang@gmail.com>
2021-01-09 23:27:57 +0800

    drawer add

--- lib/main.dart
+++ lib/main.dart
@@ -21,14 +21,32 @@ class HomeApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.green,
-        title: Text("Health Monitoring"),
-      ),
-      body: Container(
-          child: Column(
-        children: [Medicine()],
-      )),
-    );
+        drawer: Drawer(
+            child: ListView(
+          children: [
+            DrawerHeader(
+                child: Text("Side Menu"),
+                decoration: BoxDecoration(color: Colors.blue)),
+            ListTile(
+              title: Text("Item 1"),
+            ),
+            ListTile(
+              title: Text("Item 2"),
+            ),
+            ListTile(
+                title: Text("Item 3"),
+                onTap: () {
+                  Navigator.pop(context);
+                }),
+          ],
+        )),
+        appBar: AppBar(
+          backgroundColor: Colors.green,
+          title: Text("Health Monitoring"),
+        ),
+        body: Container(
+            child: Column(
+          children: [Medicine()],
+        )));
   }
 }
