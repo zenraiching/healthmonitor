8dbe5ee9ac1df72dc47b716bab510c1a8717da0c
zenraiching <rickdavidsangalang@gmail.com>
2021-01-10 00:06:39 +0800

    create  db connector

--- lib/Database/db_connect.dart
+++ lib/Database/db_connect.dart
@@ -3,5 +3,23 @@ import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 
 class DatabaseConnection {
-  setDatabase() async
+  setDatabase() async {
+    var directory = await getApplicationDocumentsDirectory();
+    var path = join(directory.path, 'db_med');
+    var database = await openDatabase(path, version: 1, onCreate: _onCreateDB);
+    return database;
+  }
+
+  _onCreateDB(Database database, int version) async {
+    await database.execute("""
+    DROP TABLE IF EXISTS tbl_medicine;
+CREATE TABLE tbl_medicine
+(id int NOT NULL PRIMARY KEY,
+medicine_name text NOT NULL,
+dosage int NOT NULL,
+units text NOT NULL,
+interval int NOT NULL,
+time timestamp NOT NULL);
+    """);
+  }
 }
