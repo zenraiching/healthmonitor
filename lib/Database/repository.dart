8d5682789cb646de8ea60e9f58f18b08fefd0c3f
zenraiching <rickdavidsangalang@gmail.com>
2021-01-10 20:31:21 +0800

    created insert data

--- lib/Database/repository.dart
+++ lib/Database/repository.dart
@@ -1,4 +1,5 @@
 import 'package:healthmonitor/database/db_connect.dart';
+import 'package:sqflite/sqlite_api.dart';
 
 class Repository {
   DatabaseConnection _dbConnection;
@@ -6,4 +7,14 @@ class Repository {
     _dbConnection = DatabaseConnection();
   }
   static Database _database;
+
+  Future<Database> get database async {
+    if (_database != null) return _database;
+    _database = await _dbConnection.setDatabase();
+    return _database;
+
+    insertData(table, data) async {
+      var connection = await database;
+    }
+  }
 }
